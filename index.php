a021fc9e34d850a35498b57994f02f14895b8a71
unknown <fotopiece@gmail.com>
2020-11-22 09:17:02 +0700

    add data

--- index.php
+++ index.php
@@ -104,8 +104,14 @@
 
 
   <script src="assets/js/dashboard.js"></script>
+
+
   <!-- End custom js for this page -->
   <?php include 'pages/end-conn.php'; ?>
+
+
+
+
   <script>
     function goPage(page) {
       window.location.href = page;
@@ -115,6 +121,131 @@
         var values = $('#selectSimType').val();
         alert(values);
       });
+    });
+  </script>
+  <style type="text/css">
+    /* Overide css code กำหนดความกว้างของปฏิทินและอื่นๆ */
+    .ui-datepicker {
+      width: 220px;
+      font-family: tahoma;
+      font-size: 11px;
+      text-align: center;
+    }
+  </style>
+  <?php
+  $jquery_ui_v = "1.8.5";
+  $theme = array(
+    "0" => "base",
+    "1" => "black-tie",
+    "2" => "blitzer",
+    "3" => "cupertino",
+    "4" => "dark-hive",
+    "5" => "dot-luv",
+    "6" => "eggplant",
+    "7" => "excite-bike",
+    "8" => "flick",
+    "9" => "hot-sneaks",
+    "10" => "humanity",
+    "11" => "le-frog",
+    "12" => "mint-choc",
+    "13" => "overcast",
+    "14" => "pepper-grinder",
+    "15" => "redmond",
+    "16" => "smoothness",
+    "17" => "south-street",
+    "18" => "start",
+    "19" => "sunny",
+    "20" => "swanky-purse",
+    "21" => "trontastic",
+    "22" => "ui-darkness",
+    "23" => "ui-lightness",
+    "24" => "vader"
+  );
+  $jquery_ui_theme = $theme[8];
+  ?>
+  <link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/<?= $jquery_ui_v ?>/themes/<?= $jquery_ui_theme ?>/jquery-ui.css" />
+  <style type="text/css">
+    /* ปรับขนาดตัวอักษรของข้อความใน tabs  
+สามารถปรับเปลี่ยน รายละเอียดอื่นๆ เพิ่มเติมเกี่ยวกับ tabs 
+*/
+    .ui-tabs {
+      font-family: tahoma;
+      font-size: 11px;
+    }
+  </style>
+  <style type="text/css">
+    /* Overide css code กำหนดความกว้างของปฏิทินและอื่นๆ */
+    .ui-datepicker {
+      width: 220px;
+      font-family: tahoma;
+      font-size: 11px;
+      text-align: center;
+    }
+  </style>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js"></script>
+  <script src="js/jqueryui_datepicker_thai.js"></script>
+  <script type="text/javascript">
+    $(function() {
+      $("#dateInput").datepicker({
+        dateFormat: 'dd-mm-yy',
+        showOn: 'button',
+        numberOfMonths: 2,
+        //		buttonImage: 'http://jqueryui.com/demos/datepicker/images/calendar.gif',
+        buttonImageOnly: false,
+        changeMonth: true,
+        changeYear: true
+      });
+
+      $("#dateInput2").datepicker({
+        dateFormat: 'dd-mm-yy',
+        showOn: 'button',
+        numberOfMonths: 2,
+        //		buttonImage: 'http://jqueryui.com/demos/datepicker/images/calendar.gif',
+        buttonImageOnly: false,
+        changeMonth: true,
+        changeYear: true
+      });
+
+      $("#dateInput3").datepicker({
+        dateFormat: 'dd-mm-yy',
+        showOn: 'button',
+        //		buttonImage: 'http://jqueryui.com/demos/datepicker/images/calendar.gif',
+        buttonImageOnly: false,
+        changeMonth: true,
+        changeYear: true
+      });
+
+      $("#openday").datepicker({
+        dateFormat: 'dd-mm-yy',
+        showOn: 'button',
+        //		buttonImage: 'http://jqueryui.com/demos/datepicker/images/calendar.gif',
+        buttonImageOnly: false,
+        changeMonth: true,
+        changeYear: true
+      });
+
+      $("#closeday").datepicker({
+        dateFormat: 'dd-mm-yy',
+        showOn: 'button',
+        //		buttonImage: 'http://jqueryui.com/demos/datepicker/images/calendar.gif',
+        buttonImageOnly: false,
+        changeMonth: true,
+        changeYear: true
+      });
+
+
+
+      $("#date_inline").datepicker({
+        dateFormat: 'dd-mm-yy',
+        buttonImageOnly: false
+      });
+
+      $("#date_inline2").datepicker({
+        dateFormat: 'dd-mm-yy',
+        buttonImageOnly: false
+      });
+
     });
   </script>
 </body>
--- pages/admin/add-number.php
+++ pages/admin/add-number.php
@@ -6,64 +6,227 @@
             <div class="col-md-2 col-xl-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">เพิ่มเบอร์</h4>
-                        <form class="forms-sample">
-                            <div class="form-group">
-                                <label>เลือกหมวดซิม</label>
-                                <select id="selectSimType" class="js-example-basic-multiple" multiple="multiple" style="width:100%">
-                                    <?php
-                                    $result = $conn->query("SELECT * FROM catalogsims");
-                                    if ($result->num_rows > 0) {
-                                        while ($row = $result->fetch_assoc()) {
-                                    ?>
-                                            <option value="<?= $row["random"]; ?>"><?= $row["name"]; ?></option>
-                                    <?php
-                                        }
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>เลือกเครือข่าย</label>
-                                <select class="js-example-basic-single" style="width:100%">
-                                    <option value="0">กรุณาเลือก</option>
-                                    <?php
-                                    $result = $conn->query("SELECT * FROM networksims order by name asc");
-                                    if ($result->num_rows > 0) {
-                                        while ($row = $result->fetch_assoc()) {
-                                    ?>
-                                            <option value="<?= $row["id"]; ?>"><?= $row["name"]; ?></option>
-                                    <?php
-                                        }
-                                    }
-                                    ?>
-                                </select>
 
-                            </div>
+                        <form class="form-sample">
+                            <p class="card-description">
+                                <h4 class="card-title">เพิ่มเบอร์</h4>
+                                <font color="red">ใส่ * หน้าหลังหากต้องการสีแดง</font>&nbsp&nbsp&nbsp
 
+                                <font color="green">ใส่ @ หน้าหลังหากต้องการสีเขียว</font>&nbsp&nbsp&nbsp
 
-
-                            <div class="form-group">
-
-                                <div class="col" style="padding-left: 0px;">
-                                    <label>เงินในซิม</label>
-                                    <div id="the-basics">
-                                        <input style="color: white;" class="typeahead" type="text" id="moneyInSim">
+                                <font color="orange">ใส่ # หน้าหลังหากต้องการสีเหลือง</font>
+                            </p>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <div class="col-sm-12">
+                                            <input type="text" style="color: white;" class="form-control">
+                                        </div>
                                     </div>
                                 </div>
-                                <br>
-                                <div class="col" style="padding-left: 0px;">
-                                    <label>เงินในซิม</label>
-                                    <div id="the-basics">
-                                        <input style="color: white;" class="typeahead" type="text">
+
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+
+                                        <label class="col-sm-3 col-form-label">เลือกหมวดซิม</label>
+                                        <div class="col-sm-9">
+                                            <select id="selectSimType" class="js-example-basic-multiple" multiple="multiple" style="width:100%">
+                                                <?php
+                                                $result = $conn->query("SELECT * FROM catalogsims");
+                                                if ($result->num_rows > 0) {
+                                                    while ($row = $result->fetch_assoc()) {
+                                                ?>
+                                                        <option value="<?= $row["random"]; ?>"><?= $row["name"]; ?></option>
+                                                <?php
+                                                    }
+                                                }
+                                                ?>
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
-                                <br>
-                                <div class="col" style="padding-left: 0px;">
-                                    <button id="btAddNumber" type="button" class="btn btn-primary mr-2">ค้นหาเบอร์</button>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">เลือกเครือข่าย</label>
+                                        <div class="col-sm-9">
+                                            <select class="js-example-basic-single" style="width:100%">
+                                                <option value="0">กรุณาเลือก</option>
+                                                <?php
+                                                $result = $conn->query("SELECT * FROM networksims order by name asc");
+                                                if ($result->num_rows > 0) {
+                                                    while ($row = $result->fetch_assoc()) {
+                                                ?>
+                                                        <option value="<?= $row["id"]; ?>"><?= $row["name"]; ?></option>
+                                                <?php
+                                                    }
+                                                }
+                                                ?>
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">เงินในซิม</label>
+                                        <div class="col-sm-9">
+                                            <input style="color: white;" class="typeahead" type="text" id="moneyInSim">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">วันที่เปิด</label>
+                                        <div class="col-sm-9">
+                                            <input style="background-color: #2a3038;color: white;" name="openday" type="text" id="openday" readonly="readonly" value="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">วันหมดอายุ</label>
+                                        <div class="col-sm-9">
+                                            <input style="background-color: #2a3038;color: white;" name="closeday" type="text" id="closeday" readonly="readonly" value="" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">ราคาต้นทุน</label>
+                                        <div class="col-sm-9">
+                                            <input style="color: white;" class="typeahead" type="text" id="baseprice">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">ราคาขาย</label>
+                                        <div class="col-sm-9">
+                                            <input style="color: white;" class="typeahead" type="text" id="price">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">หมายเหตุ</label>
+                                        <div class="col-sm-9">
+                                            <textarea class="form-control" style="color: white;" id="remark" rows="4"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary mr-2">บันทึก</button>
+                            <button class="btn btn-dark">ยกเลิก</button>
                         </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Bordered table</h4>
+                        <p class="card-description"> Add class <code>.table-bordered</code>
+                        </p>
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th> # </th>
+                                        <th> First name </th>
+                                        <th> Progress </th>
+                                        <th> Amount </th>
+                                        <th> Deadline </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td> 1 </td>
+                                        <td> Herman Beck </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $ 77.99 </td>
+                                        <td> May 15, 2015 </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 2 </td>
+                                        <td> Messsy Adam </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $245.30 </td>
+                                        <td> July 1, 2015 </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 3 </td>
+                                        <td> John Richards </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $138.00 </td>
+                                        <td> Apr 12, 2015 </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 4 </td>
+                                        <td> Peter Meggik </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $ 77.99 </td>
+                                        <td> May 15, 2015 </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 5 </td>
+                                        <td> Edward </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $ 160.25 </td>
+                                        <td> May 03, 2015 </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 6 </td>
+                                        <td> John Doe </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $ 123.21 </td>
+                                        <td> April 05, 2015 </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 7 </td>
+                                        <td> Henry Tom </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td> $ 150.00 </td>
+                                        <td> June 16, 2015 </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
